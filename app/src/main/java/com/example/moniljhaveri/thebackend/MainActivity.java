fd83e453ab97dfebf2e36e4e3f99ed8e97a995d1
Monil Jhaveri <moniljhaveri@wireless1x-155-41-97-176.bu.edu>
2015-04-11 17:29:16 -0400

    working app

--- app/src/main/java/com/example/moniljhaveri/thebackend/MainActivity.java
+++ app/src/main/java/com/example/moniljhaveri/thebackend/MainActivity.java
@@ -8,24 +8,22 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.net.URI;
 
 
 public class MainActivity extends Activity {
 
     private static String logtag = "CameraApp8";
-    private static int Take_Picture = 1;
-    private URI imageUri;
-
-
+    private static int TAKE_PICTURE = 1;
+    private Uri imageUri;
 
 
     @Override
@@ -33,44 +31,35 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button cameraButton = (Button) findViewById(R.id.buton_camera);
+        Button cameraButton = (Button) findViewById(R.id.camera_button);
         cameraButton.setOnClickListener(cameraListener);
     }
+
     private OnClickListener cameraListener = new OnClickListener() {
-        public void onClick(View v){
+        public void onClick(View v) {
             takePhoto(v);
         }
     };
-     private void takePhoto(View v){
-         Intent intent = new Intent("android.media.action.IMAGE_CAPTURe");
-         File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES,"pictures.jpeg"));
-         imageUri = Uri.fromFile(photo);
-         intent.putExtra(MediaStore.EXTRA_OUPUT, imageUri);
-         startActivityForResult(intent, TAKE_PICTURE);
-     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
-        super.onActivityResult(requestCode, resultCode, intent);
+//    private void takePhoto(View v) {
+//        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+//        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "pictures.jpeg");
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+//        imageUri = Uri.fromFile(photo);
+//        startActivityForResult(intent, TAKE_PICTURE);
+//    }
 
-        if (resultCode == Activity.RESULT_OK{
-            Uri selectedImage = ImageUri;
-            getContentResolver().notifyChange(selectImage, null);
-
-            ImageView imageView = (ImageView)findViewById(R.id.image_camera);
-            ContentResolver cr = getContentResolver();
-            Bitmap bitmap;
-
-            try {
-                 bitmap = MediaStore.Images.Media,getBitmap(cr, selectedImage);
-                 imageView.setImageBitmap(bitmap);
-                 Toast.makeText(MainActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show())
-
-            } catch(Exception e){
-                log.e(logtag. e.toString());
-            }
-        }
-
+    public void takePhoto(View v) {
+        // tell the phone we want to use the camera
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        // create a new temp file called pic.jpg in the "pictures" storage area of the phone
+        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "pic.jpg");
+        // take the return data and store it in the temp file "pic.jpg"
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+        // stor the temp photo uri so we can find it later
+        imageUri = Uri.fromFile(photo);
+        // start the camera
+        startActivityForResult(intent, TAKE_PICTURE);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -80,17 +69,27 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = imageUri;
+                    getContentResolver().notifyChange(selectedImage, null);
+                    ImageView imageView = (ImageView) findViewById(R.id.image_camera);
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(cr, selectedImage);
+                        imageView.setImageBitmap(bitmap);
+                        Toast.makeText(MainActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show();
+
+                    } catch (Exception e) {
+                        Log.e(logtag, e.toString());
+                    }
+                }
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
