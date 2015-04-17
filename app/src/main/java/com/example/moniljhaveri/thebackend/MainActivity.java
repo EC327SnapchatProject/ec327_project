dec84a742848fa7df692f1e0125662ed1f04041c
Monil Jhaveri <moniljhaveri@wireless1x-155-41-103-65.bu.edu>
2015-04-17 16:48:50 -0400

    added gallery functionality

--- app/src/main/java/com/example/moniljhaveri/thebackend/MainActivity.java
+++ app/src/main/java/com/example/moniljhaveri/thebackend/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.moniljhaveri.thebackend;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -24,7 +25,8 @@ public class MainActivity extends Activity {
     private static String logtag = "CameraApp8";
     private static int TAKE_PICTURE = 1;
     private Uri imageUri;
-
+    private static int imageGallery_load_image = 1; //this is for accessing the image gallery
+    private String selectedImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,7 @@ public class MainActivity extends Activity {
         libraryButton.setOnClickListener(libraryListener);
     }
 
+
     private OnClickListener cameraListener = new OnClickListener() {
         public void onClick(View v) {
             takePhoto(v);
@@ -45,7 +48,10 @@ public class MainActivity extends Activity {
     };
     private OnClickListener libraryListener = new OnClickListener() {
         public void onClick(View view) {
-
+            Intent intent = new Intent();
+            intent.setType("Image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), imageGallery_load_image);
         }
     };
 
@@ -59,18 +65,7 @@ public class MainActivity extends Activity {
         startActivityForResult(intent, TAKE_PICTURE);
     }
 
-//    public void takePhoto(View v) {
-//        // tell the phone we want to use the camera
-//        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-//        // create a new temp file called pic.jpg in the "pictures" storage area of the phone
-//        File photo = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "pic.jpg");
-//        // take the return data and store it in the temp file "pic.jpg"
-//        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
-//        // stor the temp photo uri so we can find it later
-//        imageUri = Uri.fromFile(photo);
-//        // start the camera
-//        startActivityForResult(intent, TAKE_PICTURE);
-//    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -84,22 +79,40 @@ public class MainActivity extends Activity {
         switch (requestCode) {
             case 1:
                 if (resultCode == Activity.RESULT_OK) {
-                    Uri selectedImage = imageUri;
-                    getContentResolver().notifyChange(selectedImage, null);
-                    ImageView imageView = (ImageView) findViewById(R.id.image_camera);
-                    ContentResolver cr = getContentResolver();
-                    Bitmap bitmap;
+                    if (requestCode == imageGallery_load_image) {
+                        Uri selectedImageUri = data.getData();
+                        selectedImagePath = getPath(selectedImageUri);
+                    } else {
+                        Uri selectedImage = imageUri;
+                        getContentResolver().notifyChange(selectedImage, null);
+                        ImageView imageView = (ImageView) findViewById(R.id.image_camera);
+                        ContentResolver cr = getContentResolver();
+                        Bitmap bitmap;
 
-                    try {
-                        bitmap = MediaStore.Images.Media.getBitmap(cr, selectedImage);
-                        imageView.setImageBitmap(bitmap);
-                        Toast.makeText(MainActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show();
+                        try {
+                            bitmap = MediaStore.Images.Media.getBitmap(cr, selectedImage);
+                            imageView.setImageBitmap(bitmap);
+                            Toast.makeText(MainActivity.this, selectedImage.toString(), Toast.LENGTH_LONG).show();
 
-                    } catch (Exception e) {
-                        Log.e(logtag, e.toString());
+                        } catch (Exception e) {
+                            Log.e(logtag, e.toString());
+                        }
                     }
-                }
 
+
+                }
         }
+    }
+    public String getPath(Uri uri1){ //This gets path of the string image
+        String res = null;
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri1, projection, null, null, null);
+        if ( cursor.moveToFirst()){;
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            res = cursor.getString(column_index);
+        }
+        cursor.close();
+        return res;
     }
 }
